a3cd371548b5f24a216ea5f99daf82007ac1d0ef
polyndrom <polyndromdev@gmail.com>
2021-02-17 09:36:52 +1000

    some refactoring

--- include/utest/utest.hpp
+++ include/utest/utest.hpp
@@ -8,16 +8,13 @@
 
 namespace utest {
     namespace detail {
-
-
-            using test_func = std::function<void(std::string_view)>;
-            using test_case = std::pair<std::string_view, test_func>;
-
-
+        using test_func = std::function<void(std::string_view)>;
+        using test_case = std::pair<std::string_view, test_func>;
         struct assertion {
             assertion(std::string_view condition, bool condition_value, std::string_view file, int line,
                       std::string_view message)
-                : condition(condition), condition_value(condition_value), file(file), line(line), message(message) {}
+                    : condition(condition), condition_value(condition_value), file(file), line(line),
+                      message(message) {}
             std::string_view what_condition() const { return condition; }
             bool is_false() const { return condition_value; }
             std::string_view what_file() const { return file; }
@@ -30,63 +27,55 @@ namespace utest {
             int line;
             std::string_view message;
         };
-
-
         inline std::vector<test_case> tests;
         inline std::map<std::string_view, std::vector<assertion>> assertions;
-
         struct auto_test_register {
             auto_test_register(std::string_view name, const test_func& func) {
                 tests.emplace_back(name, func);
                 assertions.emplace(name, std::vector<assertion>());
             }
         };
-
-        namespace {
-            void register_assert(std::string_view test_name, std::string_view condition, bool condition_value,
-                                 std::string_view file, int line, std::string_view message = "") {
-                assertions[test_name].emplace_back(condition, condition_value, file, line, message);
-            }
-            void print_assertion_info(const detail::assertion& fail) noexcept {
-                std::cerr << "Assertion failed in " << fail.what_file() << ":" << fail.what_line() << ": "
-                          << fail.what_condition() << std::endl;
-                if (!fail.what_message().empty()) {
-                    std::cerr << "    With message: " << fail.what_message() << std::endl;
-                }
+        inline void register_assert(std::string_view test_name, std::string_view condition, bool condition_value,
+                                    std::string_view file, int line, std::string_view message = "") {
+            assertions[test_name].emplace_back(condition, condition_value, file, line, message);
+        }
+        inline void print_assertion_info(const detail::assertion& fail) noexcept {
+            std::cerr << "Assertion failed in " << fail.what_file() << ":" << fail.what_line() << ": "
+                      << fail.what_condition() << std::endl;
+            if (!fail.what_message().empty()) {
+                std::cerr << "    With message: " << fail.what_message() << std::endl;
             }
         }
     }
-
-    namespace {
-        int run_tests() {
-            int passed_tests = 0;
-            // register all assertions
-            for (const auto&[name, func] : detail::tests) {
-                std::invoke(func, name);
-            }
-            for (const auto&[name, assertions] : detail::assertions) {
-                // store failed asserts
-                std::vector<std::reference_wrapper<const detail::assertion>> fails;
-                fails.reserve(assertions.size());
-                for (const auto& assertion : assertions) {
-                    if (!assertion.is_false()) {
-                        fails.emplace_back(std::cref(assertion));
-                    }
-                }
-                std::cerr << "Running " << name << "...";
-                if (fails.empty()) {
-                    std::cerr << " OK!";
-                    ++passed_tests;
-                }
-                std::cerr << std::endl;
-                // for each fail prints the fail info (fails.size() > 0)
-                for (const auto& fail : fails) {
-                    detail::print_assertion_info(fail);
-                }
-            }
-            std::cerr << "========== Passed " << passed_tests << "/" << detail::tests.size() << " tests ==========" << std::endl;
-            return static_cast<int>(detail::tests.size()) - passed_tests;
+    inline int run_tests() {
+        int passed_tests = 0;
+        // register all assertions
+        for (const auto&[name, func] : detail::tests) {
+            std::invoke(func, name);
         }
+        for (const auto&[name, assertions] : detail::assertions) {
+            // store failed asserts
+            std::vector<std::reference_wrapper<const detail::assertion>> fails;
+            fails.reserve(assertions.size());
+            for (const auto& assertion : assertions) {
+                if (!assertion.is_false()) {
+                    fails.emplace_back(std::cref(assertion));
+                }
+            }
+            std::cerr << "Running " << name << "...";
+            if (fails.empty()) {
+                std::cerr << " OK!";
+                ++passed_tests;
+            }
+            std::cerr << std::endl;
+            // for each fail prints the fail info (fails.size() > 0)
+            for (const auto& fail : fails) {
+                detail::print_assertion_info(fail);
+            }
+        }
+        std::cerr << "========== Passed " << passed_tests << "/" << detail::tests.size() << " tests =========="
+                  << std::endl;
+        return static_cast<int>(detail::tests.size()) - passed_tests;
     }
 }
 
